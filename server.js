ab141ccc6cbb0e90a7b79df66794ff01604f0f01
Leo Salvador Salvador <leofelipe.a@gmail.com>
2020-02-03 23:50:19 -0300

    configurando server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const app  = express();
 
-app.use(express.static(__dirname+'/dist/'));
+app.use(express.static(__dirname+'/dist/todo'));
 
 app.get('/*', function(req,res){
     //res.sendFile(__dirname+'/dist/index.html');
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/todo/index.html'));
 });
 
 app.listen(process.env.PORT || 4200);
